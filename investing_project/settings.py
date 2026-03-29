67b2b234d094cdacb8d9be3d9abdaa2f89583406
Georgi <georgi.manev@entra.energy>
2026-03-29 18:57:25 +0300

    up

--- investing_project/settings.py
+++ investing_project/settings.py
@@ -323,8 +323,8 @@ CELERY_RESULT_SERIALIZER = "json"
 CELERY_TIMEZONE = TIME_ZONE
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 CELERY_WORKER_CONCURRENCY = int(os.getenv("CELERY_WORKER_CONCURRENCY", "2"))
-DAILY_BRIEF_SEND_HOUR_UTC = int(os.getenv("DAILY_BRIEF_SEND_HOUR_UTC", "11"))
-DAILY_BRIEF_SEND_MINUTE_UTC = int(os.getenv("DAILY_BRIEF_SEND_MINUTE_UTC", "42"))
+DAILY_BRIEF_SEND_HOUR_UTC = int(os.getenv("DAILY_BRIEF_SEND_HOUR_UTC", "16"))
+DAILY_BRIEF_SEND_MINUTE_UTC = int(os.getenv("DAILY_BRIEF_SEND_MINUTE_UTC", "0"))
 CELERY_BEAT_SCHEDULE = {
     "send-daily-top-3-edition": {
         "task": "api.tasks.send_daily_top_3_edition",
