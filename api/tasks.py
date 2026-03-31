703cad0d1dec7d6d2b1faf53b23e7a7f38c268fc
Georgi <georgi.manev@entra.energy>
2026-03-31 16:13:12 +0300

    up

--- api/tasks.py
+++ api/tasks.py
@@ -21,7 +21,9 @@ def send_daily_top_3_edition() -> None:
 
 
 @shared_task(ignore_result=True, name="api.tasks.run_trading_view_scrape")
-def run_trading_view_scrape(*, skip_rsi: bool = False) -> None:
+def run_trading_view_scrape(skip_rsi: bool = False, **kwargs: object) -> None:
+    if "skip_rsi" in kwargs:
+        skip_rsi = bool(kwargs["skip_rsi"])
     logger.info("Starting trading_view_scrape task (skip_rsi=%s).", skip_rsi)
     call_command("trading_view_scrape", skip_rsi=skip_rsi)
     logger.info("Finished trading_view_scrape task (skip_rsi=%s).", skip_rsi)
